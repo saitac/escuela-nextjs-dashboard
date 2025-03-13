959bce74bcb5794c5589d8e2fa8814d809551483
salvador <salvadoraita@gmail.com>
2025-03-13 17:52:09 -0300

    20250313 17:52

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -17,6 +17,8 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
   const initialState: State = { message: null, errors: {} };
   const [state, formAction] = useActionState(createInvoice, initialState);
 
+  console.log(state)
+
   return (
     <form action={formAction} >
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
@@ -68,10 +70,21 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 step="0.01"
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby='amount-error'
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
+          {/*<div>{state.errors?.amount ? <p className="mt-2 text-sm text-red-500">{state.errors.amount}</p> : ""}</div>*/}
+          <div id="amount-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.amount &&
+              state.errors.amount.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+            ))}
+          </div>
+          
         </div>
 
         {/* Invoice Status */}
@@ -80,7 +93,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             Set the invoice status
           </legend>
           <div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
-            <div className="flex gap-4">
+            <div className="flex gap-4" aria-describedby='status-error'>
               <div className="flex items-center">
                 <input
                   id="pending"
@@ -112,6 +125,17 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 </label>
               </div>
             </div>
+          </div>
+          <div id="status-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.status &&
+              state.errors.status.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+            ))}
+          </div>
+          <div>
+            {state.message && <p className="mt-2 text-sm text-red-500">{state.message}</p>}
           </div>
         </fieldset>
       </div>
