2acfdf2c2fcd46eaa761cb672abdd5a5800d69ff
salvador <salvadoraita@gmail.com>
2025-03-17 12:36:05 -0300

    20250317 12:36

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from "zod";
 import postgres from "postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 
 const FormSchema = z.object({
     id: z.string(),
@@ -115,10 +117,27 @@ const deleteInvoice = async (id: string) => {
     revalidatePath('/dashboard/invoices');
 }
 
+const authenticate = async (prevState: string | undefined, formData: FormData) => {
+    try {
+        await signIn ('credentials', formData);
+      } catch (error) {
+        if (error instanceof AuthError) {
+          switch (error.type) {
+            case 'CredentialsSignin':
+              return 'Invalid credentials.';
+            default:
+              return 'Something went wrong.';
+          }
+        }
+        throw error;
+      }
+}
+
 
 export {
     createInvoice,
     updateInvoice,
     deleteInvoice,
-    type State
+    type State,
+    authenticate
 }
